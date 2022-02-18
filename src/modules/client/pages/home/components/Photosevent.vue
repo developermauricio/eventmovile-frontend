7348320feb74533e27ac378aa56d5309ad5fb0fb
developermauricio <70330439+developermauricio@users.noreply.github.com>
2022-02-18 14:32:51 -0500

    gallery home

--- src/modules/client/pages/home/components/Photosevent.vue
+++ src/modules/client/pages/home/components/Photosevent.vue
@@ -8,23 +8,25 @@
       <h3 class="text-photos-event">Fotos del evento</h3>
     </router-link>
   </div>
-  <div v-if="gallery.length === 0">
-    No hay fotografías disponibles para este evento
-  </div>
-  <div class="container slide-photo-event" v-else>
-    <div class="items-photo-event gallery-img">
-<!--      <a href="assets/img/bg-img/19.png" class="ingle-gallery-item"><img class="rounded" src="assets/img/bg-img/19.png"-->
-<!--                                                                         alt=""></a>-->
-<!--      <a href="assets/img/bg-img/19.png" class="ingle-gallery-item"><img class="rounded" src="assets/img/bg-img/19.png"-->
-<!--                                                                         alt=""></a>-->
-<!--      <a href="assets/img/bg-img/19.png" class="ingle-gallery-item"><img class="rounded" src="assets/img/bg-img/19.png"-->
-<!--                                                                         alt=""></a>-->
-      <a :href="urlBaseFile+item.picture" class="ingle-gallery-item" v-for="item in gallery" :key="item.id">
-        <img style="width:400px; height:100px;" class="rounded" :src="urlBaseFile+item.picture" alt="">
-      </a>
+  <div class="container" >
+    <div class="card" v-if="gallery.length === 0">
+      <div class="card-body">
+        <div class="container">
+          <div>
+            No hay fotografías disponibles para este evento
+          </div>
+        </div>
+      </div>
     </div>
-
+    <div class="slide-photo-event" v-if="gallery.length > 0">
+      <div class="items-photo-event gallery-img">
+        <a :href="urlBaseFile+item.picture" class="ingle-gallery-item" v-for="item in gallery" :key="item.id">
+          <img style="width:400px; height:100px;" class="rounded" :src="urlBaseFile+item.picture" alt="">
+        </a>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
@@ -41,51 +43,85 @@ export default {
     }
   },
   methods: {
-     getDataGallery(){
-       let resp = this
-       setTimeout(() =>{
-         window.axios.get(`${this.urlBase}/get-data-gallery-home/${resp.eventID}`).then(response =>{
-           resp.gallery = response.data
-           setTimeout(() =>{
-             if (document.querySelectorAll(".slide-photo-event").length > 0) {
-               window.tns({
-                 container: ".items-photo-event",
-                 items: 2,
-                 gutter: this.gallery.length,
-                 center: false,
-                 slideBy: "page",
-                 autoplay: false,
-                 fixedWidth: 150,
-                 loop: false,
-                 autoplayButtonOutput: false,
-                 autoplayTimeout: 4000,
-                 speed: 1000,
-                 mouseDrag: true,
-                 controls: false,
-                 nav: false,
-                 navPosition: "bottom",
-                 controlsContainer: document.querySelector('.controls-slide-schedule'),
-               });
-             }
+    getDataGallery() {
+      let resp = this
+      setTimeout(() => {
+        window.axios.get(`${this.urlBase}/get-data-gallery-home/${resp.eventID}`).then(response => {
+          resp.gallery = response.data
+          localStorage.setItem(
+              "galleryHome",
+              JSON.stringify(resp.gallery)
+          );
+          setTimeout(() => {
+            if (document.querySelectorAll(".slide-photo-event").length > 0) {
+              window.tns({
+                container: ".items-photo-event",
+                items: 2,
+                gutter: this.gallery.length,
+                center: false,
+                slideBy: "page",
+                autoplay: false,
+                fixedWidth: 150,
+                loop: false,
+                autoplayButtonOutput: false,
+                autoplayTimeout: 4000,
+                speed: 1000,
+                mouseDrag: true,
+                controls: false,
+                nav: false,
+                navPosition: "bottom",
+                controlsContainer: document.querySelector('.controls-slide-schedule'),
+              });
+            }
 
-             baguetteBox.run(".gallery-img", {
-               captions: true,
-               fullScreen: false,
-               animation: "slideIn", // Also available 'fadeIn' or 'false' options
-               overlayBackgroundColor: "rgba(15,7,15,0.7)"
-             });
-           }, 1000)
+            baguetteBox.run(".gallery-img", {
+              captions: true,
+              fullScreen: false,
+              animation: "slideIn", // Also available 'fadeIn' or 'false' options
+              overlayBackgroundColor: "rgba(15,7,15,0.7)"
+            });
+          }, 1000)
 
 
-         }).catch(err => {
-           console.log(err)
-         })
-       }, 50)
+        }).catch(err => {
+          console.log(err)
+        })
+      }, 50)
 
-     }
+    }
   },
   mounted() {
     this.eventID = localStorage.getItem('eventId')
+    this.gallery = JSON.parse(
+        localStorage.getItem("galleryHome") || "[]"
+    );
+    if (document.querySelectorAll(".slide-photo-event").length > 0) {
+      window.tns({
+        container: ".items-photo-event",
+        items: 2,
+        gutter: this.gallery.length,
+        center: false,
+        slideBy: "page",
+        autoplay: false,
+        fixedWidth: 150,
+        loop: false,
+        autoplayButtonOutput: false,
+        autoplayTimeout: 4000,
+        speed: 1000,
+        mouseDrag: true,
+        controls: false,
+        nav: false,
+        navPosition: "bottom",
+        controlsContainer: document.querySelector('.controls-slide-schedule'),
+      });
+    }
+
+    baguetteBox.run(".gallery-img", {
+      captions: true,
+      fullScreen: false,
+      animation: "slideIn", // Also available 'fadeIn' or 'false' options
+      overlayBackgroundColor: "rgba(15,7,15,0.7)"
+    });
     this.getDataGallery()
 
   }
